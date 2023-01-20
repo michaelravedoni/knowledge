d8b1bc61f4be5d2297fd64264284140ad4513754
Michael Ravedoni <michael@ravedoni.com>
2023-01-20 16:31:30 +0100

    fix: ui

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,9 +8,9 @@
     <section class="py-12 bg-gray-200">
         <div class="w-full max-w-3xl mx-auto px-4 sm:px-6 md:px-8 relative">
             <div class="bg-gray-50 px-6 py-12 -mt-20 rounded">
-                <article class="prose prose-lg max-w-none">
+                <article class="prose sm:prose-sm md:prose-md lg:prose-lg max-w-none">
 
-                    <div class="text-4xl font-bold dark:text-white">{{ $article->title }}</div>
+                    <div class="text-2xl lg:text-4xl font-bold dark:text-white">{{ $article->title }}</div>
 
                     @foreach ($article->content as $block)
 
@@ -18,7 +18,7 @@
                         <p class="mb-3 font-light text-gray-500 dark:text-gray-400">{!! data_get($block, 'data.content') !!}</p>
 
                         @elseif ($block['type'] == 'block')
-                        <div class="flex p-4 my-4 text-sm kn-block-{{ data_get($block, 'data.level') }}" role="alert">
+                        <div class="flex flex-col md:flex-row p-4 my-4 text-sm kn-block-{{ data_get($block, 'data.level') }}" role="alert">
                             <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3 @if (! data_get($block, 'data.heading')) mt-6 @endif" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                             <span class="sr-only">Info</span>
                             <div>
