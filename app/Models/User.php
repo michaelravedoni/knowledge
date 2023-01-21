1ff961b3da4355a6cfc2cc611fa762afcbb85ca5
Michael Ravedoni <michael@ravedoni.com>
2023-01-21 17:33:30 +0100

    fix: filament user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use App\Enums\UserRole;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -50,7 +51,7 @@ class User extends Authenticatable
 
     public function canAccessFilament(): bool
     {
-        return true;
+        return $this->hasAdminAccess();
     }
 
     public function hasAdminAccess(): bool
